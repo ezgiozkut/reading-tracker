94a5cb517aea8d2ff2e5abb5ce4502f9315d400e
ezgiozkut <127358487+ezgiozkut@users.noreply.github.com>
2023-03-09 03:38:38 +0300

    Update reading-tracker.py

--- reading-tracker.py
+++ reading-tracker.py
@@ -11,15 +11,26 @@ class ReadingTracker:
     def add_book(self, title, total_pages, start_date=datetime.date.today(), current_page=0):
         self.current_book = {'title': title, 'total_pages': total_pages, 'start_date': start_date, 'current_page': current_page}
         
-    def update_progress(self, pages_read):
+    def update_progress(self, pages_read, date=datetime.date.today()):
         if self.current_book is not None:
             self.current_book['current_page'] += pages_read
+            if date > self.current_book['start_date']:
+                days_since_start = (date - self.current_book['start_date']).days
+                pages_per_day = self.current_book['current_page'] / days_since_start
+                days_left = (self.current_book['total_pages'] - self.current_book['current_page']) / pages_per_day
+                finish_date = date + datetime.timedelta(days=days_left)
+                if finish_date < date:
+                    return f"Behind schedule! You should have finished the book by {finish_date.strftime('%b %d')}."
+                else:
+                    return f"On schedule! According to your daily reading goal, you should finish the book by {finish_date.strftime('%b %d')}."
+        else:
+            return 'No current book'
     
     def days_to_finish(self):
         if self.current_book is not None:
             days = (self.current_book['total_pages'] - self.current_book['current_page']) / self.goal
             finish_date = datetime.date.today() + datetime.timedelta(days=days)
-            return finish_date.strftime('%Y-%m-%d')
+            return finish_date.strftime('%b %d')
         else:
             return 'No current book'
     
@@ -35,13 +46,21 @@ class ReadingTracker:
         else:
             return 'No current book'
     
-    def on_track(self):
+    def on_track(self, date=datetime.date.today()):
         if self.current_book is not None:
-            days = (self.current_book['total_pages'] - self.current_book['current_page']) / self.goal
-            finish_date = datetime.date.today() + datetime.timedelta(days=days)
-            if finish_date > datetime.date.today():
-                return 'On schedule!'
+            days_since_start = (date - self.current_book['start_date']).days
+            pages_per_day = self.current_book['current_page'] / days_since_start
+            days_left = (self.current_book['total_pages'] - self.current_book['current_page']) / pages_per_day
+            finish_date = date + datetime.timedelta(days=days_left)
+            if finish_date < date:
+                return f"Behind schedule! You should have finished the book by {finish_date.strftime('%b %d')}."
             else:
-                return 'Behind schedule!'
+                return f"On schedule! According to your daily reading goal, you should finish the book by {finish_date.strftime('%b %d')}."
+        else:
+            return 'No current book'
+    
+    def start_date(self):
+        if self.current_book is not None:
+            return self.current_book['start_date'].strftime('%b %d')
         else:
             return 'No current book'
